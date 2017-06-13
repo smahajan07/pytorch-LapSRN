4b3c3c51790c90e09a20fd6a45b2288c4bc2e03c
sarthak mahajan <mahajan.sarthak28@gmail.com>
2017-06-13 17:17:08 +0530

    trying to load jpg files instead of .mat , added sample test file

--- test.py
+++ test.py
@@ -5,11 +5,12 @@ import numpy as np
 import time, math
 import scipy.io as sio
 import matplotlib.pyplot as plt
+from scipy.ndimage import imread
 
 parser = argparse.ArgumentParser(description="PyTorch LapSRN Test")
-parser.add_argument("--cuda", action="store_true", help="use cuda?")
+parser.add_argument("--cuda", action="store_true", default= True , help="use cuda?")
 parser.add_argument("--model", default="model/model_epoch_100.pth", type=str, help="model path")
-parser.add_argument("--image", default="butterfly_GT", type=str, help="image name")
+parser.add_argument("--image", default="sample", type=str, help="image name")
 parser.add_argument("--scale", default=4, type=int, help="scale factor, Default: 4")
 
 def PSNR(pred, gt, shave_border=0):
@@ -30,17 +31,17 @@ if cuda and not torch.cuda.is_available():
 
 model = torch.load(opt.model)["model"]
 
-im_gt_y = sio.loadmat("Set5/" + opt.image + ".mat")['im_gt_y']
-im_b_y = sio.loadmat("Set5/" + opt.image + ".mat")['im_b_y']
-im_l_y = sio.loadmat("Set5/" + opt.image + ".mat")['im_l_y']
+im_gt_y = imread("Set5/" + opt.image + ".jpg")
+im_b_y = imread("Set5/" + opt.image + "_LR" + ".jpg")
+# im_l_y = sio.loadmat("Set5/" + opt.image + ".mat")['im_l_y']
            
 im_gt_y = im_gt_y.astype(float)
 im_b_y = im_b_y.astype(float)
-im_l_y = im_l_y.astype(float)        
+# im_l_y = im_l_y.astype(float)
 
 psnr_bicubic = PSNR(im_gt_y, im_b_y,shave_border=opt.scale)
 
-im_input = im_l_y/255.
+im_input = im_b_y/255.
 
 im_input = Variable(torch.from_numpy(im_input).float()).view(1, -1, im_input.shape[0], im_input.shape[1])
 
@@ -72,14 +73,14 @@ print("It takes {}s for processing".format(elapsed_time))
 
 fig = plt.figure()
 ax = plt.subplot("131")
-ax.imshow(im_gt_y, cmap='gray')
+ax.imshow(im_gt_y)
 ax.set_title("GT")
 
 ax = plt.subplot("132")
-ax.imshow(im_b_y, cmap='gray')
+ax.imshow(im_b_y)
 ax.set_title("Input(Bicubic)")
 
 ax = plt.subplot("133")
-ax.imshow(im_h_y, cmap='gray')
+ax.imshow(im_h_y)
 ax.set_title("Output(LapSRN)")
 plt.show()
